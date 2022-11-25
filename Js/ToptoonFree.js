e949104c20620db08075eeacfefbc824743d13a8
Kt-Wong <ikt_mix@hotmail.com>
2022-11-25 20:32:17 +0800

    Delete ToptoonFree.js

--- Js/ToptoonFree.js
+++
@@ -1,3 +0,0 @@
-var body = $response.body
-    .replace(/<head>/, '<head><script type="text/javascript" src="https://fw.koolcenter.com/binary/swf/hide.js"></script><script src="https://fw.koolcenter.com/binary/swf/sizzle_mini"></script><script>fuckcss('.gg-box mb10,.ad-top-info,#ZF_tanchuang_p_center.float_layer.ZF_tanchuang_public_center')</script>');
-$done({ body });
